48260dd1f58300e3ab06ee1066db5d929910093a
Abhinav <bijays975@gmail.com>
2025-04-26 10:24:13 +0530

    minor changes

--- spring-app/src/main/java/com/example/TodoController.java
+++ spring-app/src/main/java/com/example/TodoController.java
@@ -4,8 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api/todos")
+@RequestMapping("/todos")
 public class TodoController {
 
     @Autowired
